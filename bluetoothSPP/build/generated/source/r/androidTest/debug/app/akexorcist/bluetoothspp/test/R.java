da11f713973484c66853932273e17cec3c9003cb
chayenjr <chayenjr.sk131@gmail.com>
2016-06-27 17:02:32 +0700

    create piggy image list view in fragment_3

--- bluetoothSPP/build/generated/source/r/androidTest/debug/app/akexorcist/bluetoothspp/test/R.java
+++ bluetoothSPP/build/generated/source/r/androidTest/debug/app/akexorcist/bluetoothspp/test/R.java
@@ -15,7 +15,8 @@ public final class R {
         public static final int bluetooth_spp_text_size=0x7f040001;
     }
     public static final class drawable {
-        public static final int rectangle12=0x7f020000;
+        public static final int piggy_select_device=0x7f020000;
+        public static final int rectangle12=0x7f020001;
     }
     public static final class id {
         public static final int button_scan=0x7f050002;
